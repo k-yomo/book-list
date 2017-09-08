488af66551acb8358df6560564783a6aadd4a7ff
Kanji Yomoda <kanji.yy@gmail.com>
2017-09-07 23:15:05 -0700

    Implement book list component

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+
+export default function () {
+  return [
+    { title: 'Jabascript: The Good Parts' },
+    { title: 'Harry Potter' },
+    { title: 'The Dark Tower' },
+    { title: 'Eloquent Ruby' }
+  ]
+}
